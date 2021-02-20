7a7779c0868277f8fad203f8c1bda0556de0f30c
Niharika <niharika.3297@gmail.com>
2021-02-20 11:18:15 +0530

    Add NFT Composer

---
+++ nft/nft_composer/nft_composer-lib.js
@@ -0,0 +1,12 @@
+///(import
+///)
+
+class nft_composer {
+    
+///(functions
+/*>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> NFT: COMPOSER  <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<*/
+
+//TODO: Add NFT Composer library functions
+
+///)
+}
---
+++ nft/nft_composer/nft_composer-page.js
@@ -0,0 +1,90 @@
+///(page-post-content
+import "../components/page-panel.js";
+import "../components/page-body.js";
+import "../../../components/action-card.js";
+import "../../../components/account-widget.js";
+import "../../../components/text-widget.js";
+import "../../../components/number-widget.js";
+import DappLib from "@decentology/dappstarter-dapplib";
+import { LitElement, html, customElement, property } from "lit-element";
+
+@customElement('nft-composer-page')
+export default class NftComposerPage extends LitElement {
+  @property()
+  title;
+  @property()
+  category;
+  @property()
+  description;
+
+  createRenderRoot() {
+    return this;
+  }
+
+  constructor(args) {
+    super(args);
+  }
+
+  render() {
+    let content = html`
+      <page-body
+        title="${this.title}"
+        category="${this.category}"
+        description="${this.description}"
+      >
+
+        <action-card
+          title="Account Query Example"
+          description="An example of how to query an account during development"
+          action="getAccountInfo"
+          method="get"
+          fields="account"
+        >
+          <account-widget
+            field="account"
+            label="Account"
+            placeholder="Account address"
+          >
+          </account-widget>
+        </action-card>
+
+
+        <action-card
+          title="Initialize Account"
+          description="Initializes an account so it can receive a NFT"
+          action="initializeAccount"
+          method="post"
+          fields="account"
+        >
+          <account-widget
+            field="account"
+            label="Account"
+            placeholder="Account address"
+          >
+          </account-widget>
+        </action-card>
+
+        <action-card
+          title="Get NFT IDs"
+          description="Get NFT IDs for Account"
+          action="getIDs"
+          method="get"
+          fields="account"
+        >
+          <account-widget
+            field="account"
+            label="Account"
+            placeholder="Account address"
+          >
+          </account-widget>
+        </action-card>
+
+
+      </page-body>
+      <page-panel id="resultPanel"></page-panel>
+    `;
+
+    return content;
+  }
+}
+///)
